3414ba01549f0f98afbe99d121688d7f241d96fb
AAshu1412 <ashutoshsanjeevmittal@gmail.com>
2024-01-11 23:59:23 +0530

    add image

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import Navbar from './components/navbar/Navbar';
 import Home from './components/home/Home';
-
+    
 function App() {
   return (
     <BrowserRouter>
+    <img src='C:\Javascript\react\freelance\src\components\assets\BG.png'></img>
       <Navbar/>
     <Routes>
     
