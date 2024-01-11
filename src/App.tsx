f7584a6f6b716401ef96aece342c5f75b11bdd3c
AAshu1412 <ashutoshsanjeevmittal@gmail.com>
2024-01-11 23:03:22 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './App.css';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <div>
+       <Home>
+       </Home>
+    </div>
+  );
+}
+
+export default App;
