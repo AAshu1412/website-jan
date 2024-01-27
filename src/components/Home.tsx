081d67021d4b78d6188882efcbeaff935ad081fc
AAshu1412 <ashutoshsanjeevmittal@gmail.com>
2024-01-27 23:42:09 +0530

    home update 3

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -31,16 +31,19 @@ function Home() {
           </h1>
         </div>
         <div className="flex flex-row justify-center mt-16">
-          <div className="flex flex-col item-center gap-2">
+          <div className="grid grid-cols-1 gap-2 ">
             <h1 className="text-xs text-white">Discover more</h1>
-            <img src={scroll} className="w-5"></img>
+            <div className="flex justify-center">
+              {" "}
+              <img src={scroll} className="w-5"></img>
+            </div>
           </div>
         </div>
       </div>
       {/*--------------------------------------------------------------------------------------------------------*/}
 
-      <div className=" flex flex-row justify-center ">
-        <div>
+      <div className=" flex flex-row justify-center mt-16 ">
+        <div className="grid gap-16">
           <div className="flex flex-col">
             <h1 className="w-28 p-1 text-sm font-bold text-green-950 bg-green-400 ">
               What we offer
@@ -57,19 +60,19 @@ function Home() {
             </h1>
           </div>
           {/*-------------------------------------------------------------------------------------------------------------*/}
-          <div className="grid grid-cols-4 gap-10">
-            <div className="w-auto border-solid border-orange-500 border-2		">
+          <div className="grid grid-cols-4 gap-4">
+            <div className="w-auto box-border border-solid  border-2 grid gap-2 ">
               <img src={icon} />
-              <div className="w-60">
-                <h1 className="font-bold text-xl bg-slate-600">
+              <div className="w-60 grid gap-2">
+                <h1 className="font-bold text-xl ">
                   Embedded system Solutions
                 </h1>
-                <h1 className="bg-pink-600">
+                <h1>
                   We craft embedded applications tailored to diverse market
                   needs, ensuring that your products are always ahead of the
                   curve.
                 </h1>
-                <div className="flex flex-row bg-yellow-500 gap-3">
+                <div className="flex flex-row  gap-3">
                   {" "}
                   <h1 className="text-sm	font-bold	text-green-800">
                     Learn More{" "}
@@ -78,60 +81,136 @@ function Home() {
                 </div>
               </div>
             </div>
-            <div>
+            <div className="w-auto box-border border-solid  border-2 grid gap-2 ">
               <img src={icon} />
-              <h1>Storage Solutions</h1>
-              <h1 className="w-64">
-              From SSD firmware to PCIe fabric-based storage chassis and system management software, our solutions are designed for speed, reliability, and scalability.
-              </h1>
-              <h1>Learn More</h1>
+              <div className="w-60 grid gap-2">
+                <h1 className="font-bold text-xl ">Storage Solutions</h1>
+                <h1>
+                  From SSD firmware to PCIe fabric-based storage chassis and
+                  system management software, our solutions are designed for
+                  speed, reliability, and scalability.
+                </h1>
+                <div className="flex flex-row  gap-3">
+                  {" "}
+                  <h1 className="text-sm	font-bold	text-green-800">
+                    Learn More{" "}
+                  </h1>
+                  <img src={arrow2} className="w-4" />
+                </div>
+              </div>
             </div>
-            <div>
+            <div className="w-auto box-border border-solid  border-2 grid gap-2 ">
               <img src={icon} />
-              <h1>Internet of Things</h1>
-              <h1 className="w-64">
-              From SSD firmware to PCIe fabric-based storage chassis and system management software, our solutions are designed for speed, reliability, and scalability.
-              </h1>
-              <h1>Learn More</h1>
+              <div className="w-60 grid gap-2">
+                <h1 className="font-bold text-xl ">Internet of Things </h1>
+                <h1>
+                  From SSD firmware to PCIe fabric-based storage chassis and
+                  system management software, our solutions are designed for
+                  speed, reliability, and scalability.
+                </h1>
+                <div className="flex flex-row  gap-3">
+                  {" "}
+                  <h1 className="text-sm	font-bold	text-green-800">
+                    Learn More{" "}
+                  </h1>
+                  <img src={arrow2} className="w-4" />
+                </div>
+              </div>
             </div>
-            <div>
+            <div className="w-auto box-border border-solid  border-2 grid gap-2 ">
               <img src={icon} />
-              <h1>Quality Assurance</h1>
-              <h1 className="w-64">
-              From SSD firmware to PCIe fabric-based storage chassis and system management software, our solutions are designed for speed, reliability, and scalability.
-              </h1>
-              <h1>Learn More</h1>
+              <div className="w-60 grid gap-2">
+                <h1 className="font-bold text-xl ">Quality Assurance </h1>
+                <h1>
+                  From SSD firmware to PCIe fabric-based storage chassis and
+                  system management software, our solutions are designed for
+                  speed, reliability, and scalability.
+                </h1>
+                <div className="flex flex-row  gap-3">
+                  {" "}
+                  <h1 className="text-sm	font-bold	text-green-800">
+                    Learn More{" "}
+                  </h1>
+                  <img src={arrow2} className="w-4" />
+                </div>
+              </div>
             </div>
-            <div>
+            <div className="w-auto box-border border-solid  border-2 grid gap-2 ">
               <img src={icon} />
-              <h1>Android Platform Solutions</h1>
-              <h1 className="w-64">
-              We harness the power of the Android platform to develop advanced products that resonate with your target audience.
-              </h1>
-              <h1>Learn More</h1>
+              <div className="w-60 grid gap-2">
+                <h1 className="font-bold text-xl ">
+                  Android Platform Solutions
+                </h1>
+                <h1>
+                  We harness the power of the Android platform to develop
+                  advanced products that resonate with your target audience.
+                </h1>
+                <div className="flex flex-row  gap-3">
+                  {" "}
+                  <h1 className="text-sm	font-bold	text-green-800">
+                    Learn More{" "}
+                  </h1>
+                  <img src={arrow2} className="w-4" />
+                </div>
+              </div>
             </div>
-            <div>
+            <div className="w-auto box-border border-solid  border-2 grid gap-2 ">
               <img src={icon} />
-              <h1>AI Solutions for the Future</h1>
-              <h1 className="w-64">
-              We provide End to end system level work for AI platform and help you leverage the power of AI to stay competitive and innovative.              </h1>
-              <h1>Learn More</h1>
+              <div className="w-60 grid gap-2">
+                <h1 className="font-bold text-xl ">
+                  AI Solutions for the Future{" "}
+                </h1>
+                <h1>
+                  We provide End to end system level work for AI platform and
+                  help you leverage the power of AI to stay competitive and
+                  innovative.
+                </h1>
+                <div className="flex flex-row  gap-3">
+                  {" "}
+                  <h1 className="text-sm	font-bold	text-green-800">
+                    Learn More{" "}
+                  </h1>
+                  <img src={arrow2} className="w-4" />
+                </div>
+              </div>
             </div>
-            <div>
+            <div className="w-auto box-border border-solid  border-2 grid gap-2 ">
               <img src={icon} />
-              <h1>SoC bring-up & system level IP validation</h1>
-              <h1 className="w-64">
-              Porting Linux Kernel/RTOS. Pre & Post Silicon IP validation (Customer FPGA/reference platform)
-              </h1>
-              <h1>Learn More</h1>
+              <div className="w-60 grid gap-2">
+                <h1 className="font-bold text-xl ">
+                  SoC bring-up & system level IP validation{" "}
+                </h1>
+                <h1>
+                  We provide End to end system level work for AI platform and
+                  help you leverage the power of AI to stay competitive and
+                  innovative.
+                </h1>
+                <div className="flex flex-row  gap-3">
+                  {" "}
+                  <h1 className="text-sm font-bold	text-green-800">
+                    Learn More{" "}
+                  </h1>
+                  <img src={arrow2} className="w-4" />
+                </div>
+              </div>
             </div>
-            <div>
+            <div className="w-auto box-border border-solid border-2 grid gap-2 ">
               <img src={icon} />
-              <h1>Hardware design</h1>
-              <h1 className="w-64">
-              We provide End to end system level work for AI platform and help you leverage the power of AI to stay competitive and innovative.
-              </h1>
-              <h1>Learn More</h1>
+              <div className="w-60 grid gap-2">
+                <h1 className="font-bold text-xl ">Hardware design</h1>
+                <h1>
+                  We provide End to end system level work for AI platform and
+                  help you leverage the power of AI to stay competitive and
+                  innovative.
+                </h1>
+                <div className="flex flex-row  gap-3">
+                  {" "}
+                  <h1 className="text-sm font-bold	text-green-800">
+                    Learn More{" "}
+                  </h1>
+                  <img src={arrow2} className="w-4" />
+                </div>
+              </div>
             </div>
           </div>
         </div>
