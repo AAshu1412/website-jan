567a21c72637870e577b98b6dca201c4ed9c73f4
AAshu1412 <ashutoshsanjeevmittal@gmail.com>
2024-02-01 22:56:36 +0530

    footer update

--- src/components/home/Footer.tsx
+++ src/components/home/Footer.tsx
@@ -1,29 +1,70 @@
 import React from "react";
-import home_mail from "../../assets/home_footer/ic_mail.svg"
+import home_mail from "../../assets/home_footer/ic_mail.svg";
 import home_call from "../../assets/home_footer/ic_call.svg";
 
-function Footer(){
-return (
+function Footer() {
+  return (
     <div className="bg-[#004642] text-white ">
-        <div className="flex flex-row justify-around">
+      <div className="flex flex-row justify-around">
         <div className="w-64 flex flex-col gap-5">
-            <h1 className="text-sm text-[#FFFFFF99]">“Our mission is to provide the highest business value to our customers and live by a value-focused, technology centered organization.”</h1>
-            <div className="flex gap-2">
-                <img src={home_mail} className="w-5"/>
-                <h1 className="text-sm">hr@gytechs.com</h1>
-            </div>
-            <div className="flex gap-2">
-                <img src={home_call} className="w-5"/>
-                <h1 className="text-sm">+91 40 40177887</h1>
-            </div>
+          <h1 className="text-sm text-[#FFFFFF99]">
+            “Our mission is to provide the highest business value to our
+            customers and live by a value-focused, technology centered
+            organization.”
+          </h1>
+          <div className="flex gap-2">
+            <img src={home_mail} className="w-5" />
+            <h1 className="text-sm">hr@gytechs.com</h1>
+          </div>
+          <div className="flex gap-2">
+            <img src={home_call} className="w-5" />
+            <h1 className="text-sm">+91 40 40177887</h1>
+          </div>
         </div>
         <div>
-            2nd column
+          <div>
+            <ul>
+              <li className="font-bold	">Company</li>
+              <div className="text-sm text-[#FFFFFF99]">
+                {" "}
+                <li>Home</li>
+                <li>Services</li>
+                <li>About</li>
+                <li>Contact</li>
+                <li>Careers</li>
+              </div>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li className="font-bold	">Services</li>
+              <div className="text-sm text-[#FFFFFF99]">
+                <li>Embedded system solutions</li>
+                <li>Storage solutions</li>
+                <li>Internet of things</li>
+                <li>Quality assurance</li>
+                <li>Android platform solution</li>
+                <li>AI solutions</li>
+                <li>SOC bring-up & system</li>
+                <li>IP validation</li>
+                <li>Hardware design</li>
+              </div>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li className="font-bold	">Discover</li>
+              <div className="text-sm text-[#FFFFFF99]">
+                <li>FAQs</li>
+                <li>Testimonials</li>
+                <li>Employee Portal</li>
+              </div>
+            </ul>
+          </div>
         </div>
-        </div>
-        
+      </div>
     </div>
-);
+  );
 }
 
 export default Footer;
